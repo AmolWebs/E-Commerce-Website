4e975a8c0634d66f8cfb893cd060d1568d5a5bc3
Amol D <amoldangat836@gmail.com>
2025-03-05 13:58:53 +0530

    back and hero adjusted

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row border-gray-400 border' >
+    <div className='flex flex-col justify-between sm:flex-row border-gray-400 border' >
       {/* Hero left side */}
       <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0' >
         <div className='text-[#414141]' >
@@ -15,12 +15,16 @@ const Hero = () => {
           <div className='flex items-center gap-2' >
             <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
             <p className='w-8 md:w-11 h-[2px] bg-[#414141]' ></p>
-
           </div>
         </div>
       </div>
-      {/* Hero right side  */}
-      {/* <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" /> */}
+
+      {/* <div className="relative flex justify-end w-60 sm:w-1/3">
+        <video className="w-full object-cover h-[90vh]" autoPlay muted loop playsInline>
+          <source src="/ecom-hero-video.mp4" type="video/mp4" />
+        </video>
+      </div> */}
+
       <video className="bg-red-100 w-1/2 sm:w-1/2 h-[80vh] object-fill" autoPlay muted loop playsInline>
         <source src="/ecom-hero-video.mp4" type="video/mp4" />
       </video>
