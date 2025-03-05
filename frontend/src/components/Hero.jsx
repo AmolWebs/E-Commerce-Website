67a9f4b4c7d7315fc811517267d9ba075bcae78b
Amol D <amoldangat836@gmail.com>
2025-03-05 15:02:03 +0530

    hero adjusted

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='flex flex-col justify-between sm:flex-row border-gray-400 border' >
-      {/* Hero left side */}
+      {/* { 
+      // Hero left side
       <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0' >
         <div className='text-[#414141]' >
           <div className='flex items-center gap-2' >
@@ -17,7 +18,7 @@ const Hero = () => {
             <p className='w-8 md:w-11 h-[2px] bg-[#414141]' ></p>
           </div>
         </div>
-      </div>
+      </div> } */}
 
       {/* <div className="relative flex justify-end w-60 sm:w-1/3">
         <video className="w-full object-cover h-[90vh]" autoPlay muted loop playsInline>
@@ -25,9 +26,23 @@ const Hero = () => {
         </video>
       </div> */}
 
-      <video className="bg-red-100 w-1/2 sm:w-1/2 h-[80vh] object-fill" autoPlay muted loop playsInline>
+      {/* <video className="bg-red-100 w-1/2 sm:w-1/2 h-[80vh] object-fill" autoPlay muted loop playsInline>
         <source src="/ecom-hero-video.mp4" type="video/mp4" />
-      </video>
+      </video> */}
+
+
+<div className="flex justify-center items-center w-screen h-[70vh] overflow-hidden bg-black">
+  <video
+    className="rotate-90 w-[100vh] h-[1000vw] object-contain"
+    autoPlay
+    muted
+    loop
+    playsInline
+  >
+    <source src="/ecom-hero-video.mp4" type="video/mp4" />
+  </video>
+</div>
+
 
 
     </div>
