57467c092b8e8c6c4f68cc7794e18f7d34aede5a
Amol D <amoldangat836@gmail.com>
2025-03-18 21:28:59 +0530

    description removed

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -134,7 +134,7 @@ const Product = () => {
       <div className='mt-20' >
         <div className="flex">
           <b className='border px-5 py-3 text-sm' >Description</b>
-          <p className='border px-5 py-3 text-sm' >Reviews (122)</p>
+          
         </div>
         <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quis natus quas quam labore. Ducimus aliquid beatae minima, eos enim corporis expedita ullam dolores sunt quo accusantium dolorum ex. Natus!</p>
